10fdd08a1febd5a8bc1521fdff3697e2e2c4cacd
mrnzdev <contact@mrnz.dev>
2024-01-04 19:43:26 -0300

    Update heading text to uppercase

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="h-full m-0 flex justify-center items-center">
-      <h1 className="text-center">Coming Soon...</h1>
+      <h1 className="text-center">COMING SOON...</h1>
     </main>
   )
 }
