f017bf91195268f5f8c90db4cba71c7a192eddb1
mrnzdev <contact@mrnz.dev>
2024-01-07 19:39:22 -0300

    Update font in layout and add Geist package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { GeistMono } from 'geist/font/mono';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Neat Mail',
@@ -15,8 +13,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang='en'>
-			<body className={`${inter.className} bg-black text-white h-dvh`}>{children}</body>
+		<html className={`${GeistMono.className} lang='en'`}>
+			<body className=' bg-black text-white h-dvh'>{children}</body>
 		</html>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+				mono: ['var(--font-geist-mono)'],
+			},
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
