9fa349f19511bb7e3c0ca0625d8c8c5260f83ea7
mrnzdev <contact@mrnz.dev>
2024-01-23 15:03:38 -0300

    Add sign-in component and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import { Header } from '@/components/header';
+"use client";
+
+import { useEffect } from "react";
+import { Header } from "@/components/header";
+import SignIn from "@/components/sign-in";
 
 export default function Home() {
+	useEffect(() => {
+		const script = document.createElement("script");
+		script.src = "https://accounts.google.com/gsi/client";
+		script.async = true;
+		script.defer = true;
+
+		document.body.appendChild(script);
+
+		return () => {
+			document.body.removeChild(script);
+		};
+	}, []);
+
 	return (
 		<>
 			<Header />
+			<SignIn />
 		</>
 	);
 }
